183a408c8996e6afbea9650c61cd2d0b14396f1f
Liyi <xmage211120@gmail.com>
2020-06-26 17:36:01 +0800

    fix responsive bugs in safari

---
+++ build/precache-manifest.d48d9ae5318335e876e4720a38dee613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e2604045c8a74cf1dcafcfc70b30ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "453c83ad9bb8d571a367",
+    "url": "/static/css/main.9f37ebb7.chunk.css"
+  },
+  {
+    "revision": "48e22e68df513a14ac33",
+    "url": "/static/js/2.630a52fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.630a52fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453c83ad9bb8d571a367",
+    "url": "/static/js/main.658afc2a.chunk.js"
+  },
+  {
+    "revision": "a57451c65f890ff58a5b",
+    "url": "/static/js/runtime-main.9e92f7f9.js"
+  }
+]);
